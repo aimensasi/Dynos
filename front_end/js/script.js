b462e5c1d44c80f7fe18a17ead4c1cc5c90c1c0e
aimensasi <ali.sasi@ymail.com>
2016-12-14 10:28:31 +0800

    added front end side file

---
+++ front_end/js/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+	$('.age-range').jRange({
+    from: 2,
+    to: 19,
+    step: 1,
+    scale: ['From', 'To'],
+    format: '%s',
+    width: 900,
+    showLabels: true,
+    isRange : true,
+    snap: true
+	});	
+
+	$('.custom-checkbox').prop('indeterminate', false);
+
+});
+
