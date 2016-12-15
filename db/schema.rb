e65f8ba6040689ca3f0584df2eb13bdb22fdac16
aimensasi <ali.sasi@ymail.com>
2016-12-15 17:28:57 +0800

    fixed a problem with braintree being called when not needed which throw an exception

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20161215071134) do
 
   # These are extensions that must be enabled in order to support this database
@@ -62,11 +61,11 @@ ActiveRecord::Schema.define(version: 20161215071134) do
     t.string   "location"
     t.string   "category"
     t.string   "avatar"
-    t.integer  "reviews"
+    t.integer  "reviews",     default: 0
     t.integer  "min_age"
     t.integer  "max_age"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
     t.integer  "user_id"
   end
 
